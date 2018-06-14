160b6831747a92f43fc0dbcce8dc95c3988b8bba
DStas <stasdavid@yahoo.com>
2018-06-14 19:32:59 +0200

    - PabloDraw: Added extra zoom levels until 700% when drawing in both textmode as rip mode - PabloDraw.Console: Added AutoSave and Backup functionalities

--- Source/PabloDraw.Console/CommandHandlers/ServerCommandLine.cs
+++ Source/PabloDraw.Console/CommandHandlers/ServerCommandLine.cs
@@ -10,185 +10,242 @@ using System.Threading;
 
 namespace PabloDraw.Console.CommandHandlers
 {
-	public class ServerCommandLine : CommandLineHandler, IClientDelegate
-	{
-		public override string Name { get { return "Server"; } }
+    public class ServerCommandLine : CommandLineHandler, IClientDelegate
+    {
+        private Timer _autoSaveTimer;
+        private int _autosaveInterval;
+        private bool _backup = false;
+        private int _port;
 
-		public bool EditMode { get { return true; } }
+        public override string Name { get { return "Server"; } }
 
-		public override void GetHelp(ProcessCommandLineArgs args)
-		{
-			args.WriteOption("--server, -s", "Starts a pablo server");
-			args.WriteOption("--file=<FILE>, [input.ext]", "Initial file to load (or create) after starting the server");
-			args.WriteOption("--port=<PORT>", "Specifies the port to use (default:14400)");
-			args.WriteOption("--nat=[true|false]", "Map the port on the router using UPnP or NAT-PMP");
-			args.WriteOption("--password, -pw=<PASS>", "Sets the password for regular users");
-			args.WriteOption("--oppassword, -op=<PASS>", "Sets the password for operator access");
-			args.WriteOption("--userlevel, -ul=[viewer|editor|operator]", "Sets the user level for users (default:viewer)");
-			args.WriteOption("--autosave=<seconds>", "Interval to save the file in seconds, or 0 to turn off");
-			args.WriteOption("--backup", "Backup existing file before saving");
-		}
+        public bool EditMode { get { return true; } }
 
-		public DocumentInfoCollection DocumentInfos { get; private set; }
+        public override void GetHelp(ProcessCommandLineArgs args)
+        {
+            args.WriteOption("--server, -s", "Starts a pablo server");
+            args.WriteOption("--file=<FILE>, [input.ext]", "Initial file to load (or create) after starting the server");
+            args.WriteOption("--port=<PORT>", "Specifies the port to use (default:14400)");
+            args.WriteOption("--nat=[true|false]", "Map the port on the router using UPnP or NAT-PMP");
+            args.WriteOption("--password, -pw=<PASS>", "Sets the password for regular users");
+            args.WriteOption("--oppassword, -op=<PASS>", "Sets the password for operator access");
+            args.WriteOption("--userlevel, -ul=[viewer|editor|operator]", "Sets the user level for users (default:viewer)");
+            args.WriteOption("--autosave=<seconds>", "Interval to save the file in seconds, or 0 to turn off");
+            args.WriteOption("--backup", "Backup existing file before saving");
+        }
 
-		public Document Document { get; private set; }
+        public DocumentInfoCollection DocumentInfos { get; private set; }
 
-		public Handler Handler { get; private set; }
+        public Document Document { get; private set; }
 
-		public Client Client { get; private set; }
+        public Handler Handler { get; private set; }
 
-		public Server Server { get; private set; }
+        public Client Client { get; private set; }
 
-		public bool EnableBackups { get; set; }
+        public Server Server { get; private set; }
 
-		public ServerCommandLine()
-		{
-			DocumentInfos = DocumentInfoCollection.Default;
-		}
+        public bool EnableBackups { get; set; }
 
-		IEnumerable<ICommand> Commands
-		{
-			get
-			{
-				yield return new Commands.NewFile(this);
-				yield return new Commands.EditFile(this);
-				if (Handler != null)
-					foreach (var command in Handler.Commands)
-						yield return command;
-			}
-		}
+        public ServerCommandLine()
+        {
+            DocumentInfos = DocumentInfoCollection.Default;
+        }
 
-		IEnumerable<ICommand> ServerCommands
-		{
-			get
-			{
-				if (Handler != null)
-					foreach (var command in Handler.ServerCommands)
-						yield return command;
-			}
-		}
+        IEnumerable<ICommand> Commands
+        {
+            get
+            {
+                yield return new Commands.NewFile(this);
+                yield return new Commands.EditFile(this);
+                if (Handler != null)
+                    foreach (var command in Handler.Commands)
+                        yield return command;
+            }
+        }
 
-		public void SetDocument(Document document)
-		{
-			Document = document;
+        IEnumerable<ICommand> ServerCommands
+        {
+            get
+            {
+                if (Handler != null)
+                    foreach (var command in Handler.ServerCommands)
+                        yield return command;
+            }
+        }
 
-			Handler = document.CreateHandler();
-			Handler.ClientDelegate = this;
-			if (Client != null)
-				Client.SetCommands(Commands);
-			
-			if (Server != null)
-				Server.SetCommands(ServerCommands);
-			
-			Handler.PreLoad(null, document.LoadedFormat);
-			Handler.Loaded();
-		}
+        public void SetDocument(Document document)
+        {
+            Document = document;
 
-		public void LoadFile(string fileName, System.IO.Stream stream, bool editMode, Format format = null)
-		{
-			format = format ?? DocumentInfos.FindFormat(fileName) ?? DocumentInfos.DefaultFormat;
-			if (format == null)
-				throw new  ArgumentOutOfRangeException("fileName", "Could not find format of the file");
-			var doc = format.Info.Create();
-			doc.FileName = fileName;
-			doc.Load(stream, format, null);
-			doc.EditMode = editMode;
-			SetDocument(doc);
-		}
+            Handler = document.CreateHandler();
+            Handler.ClientDelegate = this;
+            if (Client != null)
+                Client.SetCommands(Commands);
 
-		public void StartServer(int port, string password, string operatorPassword, bool nat, UserLevel defaultUserLevel, TextWriter writer)
-		{
-			Client = new Client
-			{
-				Port = port,
-				Delegate = this,
-				Password = operatorPassword ?? password,
-				Headless = true
-			};
-			Client.SetCommands(Commands);
-			
-			Client.CurrentUser.Alias = "admin";
-			Client.CurrentUser.Level = UserLevel.Operator;
-			
-			Client.Message += (sender, e) => writer.WriteLine(e.DisplayMessage);
-			
-			Server = new Server
-			{
-				Port = port,
-				Delegate = this,
-				Password = password,
-				OperatorPassword = operatorPassword,
-				AutoMapPort = nat,
-				DefaultUserLevel = defaultUserLevel,
-				Client = Client
-			};
-			Server.SetCommands(ServerCommands);
-			Server.Start();
-			Client.Start();
-		}
+            if (Server != null)
+                Server.SetCommands(ServerCommands);
 
-		public void StopServer()
-		{
-			Client.Stop();
-			Server.Stop();
-		}
+            Handler.PreLoad(null, document.LoadedFormat);
+            Handler.Loaded();
+        }
 
-		public override bool Process(ProcessCommandLineArgs args)
-		{
-			var command = args.Command;
-			if (command.GetBool("server", "s") != true)
-				return false;
+        public void LoadFile(string fileName, System.IO.Stream stream, bool editMode, Format format = null)
+        {
+            format = format ?? DocumentInfos.FindFormat(fileName) ?? DocumentInfos.DefaultFormat;
+            if (format == null)
+                throw new ArgumentOutOfRangeException("fileName", "Could not find format of the file");
+            var doc = format.Info.Create();
+            doc.FileName = fileName;
+            doc.Load(stream, format, null);
+            doc.EditMode = editMode;
+            SetDocument(doc);
+        }
 
-			EnableBackups = command.GetBool("backup") ?? true;
-			var password = command.GetValue("password", "pw");
-			var operatorPassword = command.GetValue("op", "oppassword", "oppw");
-			var port = command.GetInt("port") ?? 14400;
-			var nat = command.GetBool("nat") ?? false;
-			var userLevel = command.GetEnum<UserLevel>("userlevel", "ul") ?? UserLevel.Viewer;
+        public void StartServer(int port, string password, string operatorPassword, bool nat, UserLevel defaultUserLevel, TextWriter writer)
+        {
+            Client = new Client
+            {
+                Port = port,
+                Delegate = this,
+                Password = operatorPassword ?? password,
+                Headless = true
+            };
+            Client.SetCommands(Commands);
 
-			var fileName = command.GetValue("file", "generic:0");
-			if (!string.IsNullOrEmpty(fileName))
-			{
-				if (File.Exists(fileName))
-				{
-					using (var stream = File.OpenRead(fileName))
-					{
-						LoadFile(fileName, stream, true);
-					}
-				}
-				else
-				{
-					var format = DocumentInfos.FindFormat(fileName);
-					if (format == null)
-						throw new InvalidOperationException(string.Format("Unrecognized format for file name '{0}'", fileName));
-					var doc = format.Info.Create();
-					doc.FileName = fileName;
-					doc.EditMode = true;
-					SetDocument(doc);
-				}
-			}
+            Client.CurrentUser.Alias = "admin";
+            Client.CurrentUser.Level = UserLevel.Operator;
 
-			var autosaveInterval = command.GetInt("autosave") ?? 0;
-			if (autosaveInterval > 0)
-			{
-				// turn on autosaving
-			}
+            Client.Message += (sender, e) => writer.WriteLine(e.DisplayMessage);
 
-			var backup = command.GetBool("backup") ?? false;
-			if (backup)
-			{
-				// turn on backups
-			}
+            Server = new Server
+            {
+                Port = port,
+                Delegate = this,
+                Password = password,
+                OperatorPassword = operatorPassword,
+                AutoMapPort = nat,
+                DefaultUserLevel = defaultUserLevel,
+                Client = Client
+            };
+            Server.SetCommands(ServerCommands);
+            Server.Start();
+            Client.Start();
+        }
 
-			args.Writer.WriteLine("Starting PabloDraw server on port {0}...", port);
-			StartServer(port, password, operatorPassword, nat, userLevel, args.Writer);
+        public void StopServer()
+        {
+            Client.Stop();
+            Server.Stop();
+        }
 
-			args.Writer.WriteLine("Started! Press any key to stop");
-			System.Console.Read();
+        public override bool Process(ProcessCommandLineArgs args)
+        {
+            var command = args.Command;
+            if (command.GetBool("server", "s") != true)
+                return false;
 
-			args.Writer.WriteLine("Stopping...");
-			StopServer();
-			return true;
-		}
-	}
+            EnableBackups = command.GetBool("backup") ?? true;
+            var password = command.GetValue("password", "pw");
+            var operatorPassword = command.GetValue("op", "oppassword", "oppw");
+            _port = command.GetInt("port") ?? 14400;
+            var nat = command.GetBool("nat") ?? false;
+            var userLevel = command.GetEnum<UserLevel>("userlevel", "ul") ?? UserLevel.Viewer;
+
+            var fileName = command.GetValue("file", "generic:0");
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                if (File.Exists(fileName))
+                {
+                    using (var stream = File.OpenRead(fileName))
+                    {
+                        LoadFile(fileName, stream, true);
+                    }
+                }
+                else
+                {
+                    var format = DocumentInfos.FindFormat(fileName);
+                    if (format == null)
+                        throw new InvalidOperationException(string.Format("Unrecognized format for file name '{0}'", fileName));
+                    var doc = format.Info.Create();
+                    doc.FileName = fileName;
+                    doc.EditMode = true;
+                    SetDocument(doc);
+                }
+            }
+
+            _autosaveInterval = command.GetInt("autosave") ?? 0;
+            if (_autosaveInterval > 0)
+            {
+                if (!Directory.Exists(_port.ToString()))
+                {
+                    Directory.CreateDirectory(_port.ToString()); // Store AutoSave in its port number folder so that multiple instances don't overwrite each other.
+                }
+                _autosaveInterval *= 1000; // Convert to seconds.
+                _autoSaveTimer = new Timer(TimerCallback, null, _autosaveInterval, Timeout.Infinite);
+            }
+
+            _backup = command.GetBool("backup") ?? false;
+
+
+            args.Writer.WriteLine("Starting PabloDraw server on port {0}...", _port);
+            StartServer(_port, password, operatorPassword, nat, userLevel, args.Writer);
+
+            args.Writer.WriteLine("Started! Press any key to stop");
+            System.Console.Read();
+
+            args.Writer.WriteLine("Stopping...");
+            StopServer();
+            return true;
+        }
+
+        private void TimerCallback(Object state)
+        {
+            try
+            {
+                if (Handler != null && 1 == 1 /*Handler.Document.IsModified*/) // TODO - IsModified is always false, even if document is changed
+                {
+                    string file;
+
+                    if (Document.LoadedFormat == null)
+                    {
+                        Document.LoadedFormat = Document.Info.DefaultFormat; // Set to Rip.
+                        file = Path.Combine(_port.ToString(), "AutoSave.rip");
+                    }
+                    else
+                    {
+                        file = Path.Combine(_port.ToString(), "AutoSave.ans");
+                    }
+
+                    if (_backup)
+                    {
+                        Handler.SaveWithBackup(file, Document.LoadedFormat);
+                        System.Console.WriteLine(string.Format("AutoSave - {0} was saved at {1} with Backup", Handler.Document.FileName, DateTime.Now));
+                    }
+                    else
+                    {
+                        Handler.Save(file, Document.LoadedFormat);
+                        System.Console.WriteLine(string.Format("AutoSave - {0} was saved at {1}", Handler.Document.FileName, DateTime.Now));
+                    }
+
+                    Handler.Document.IsModified = false;
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+#if DEBUG
+	            throw;
+#endif
+            }
+            finally
+            {
+
+                // Make sure timer is set again.
+                _autoSaveTimer.Change(_autosaveInterval, Timeout.Infinite);
+            }
+        }
+
+    }
 }
